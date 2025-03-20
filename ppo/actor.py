230743cb1177708cc1ecba58a3c34f12ae8c3f62
Adithya Rao <adithya.rao@outlook.com>
2025-03-20 09:16:01 -0500

    Set up base PPO. Need to adapt to current use case.

---
+++ ppo/actor.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+class Actor(nn.Module):
+    def __init__(self, state_dim, action_dim, max_action):
+        super(Actor, self).__init__()
+
+        self.l1 = nn.Linear(state_dim, 256)
+        self.l2 = nn.Linear(256, 256)
+        self.l3 = nn.Linear(256, action_dim)
+        
+        self.max_action = max_action
+
+    def forward(self, state):
+        if isinstance(state, np.ndarray):
+            state = torch.from_numpy(state).float()
+
+        a = F.relu(self.l1(state))
+        a = F.relu(self.l2(a))
+        a = torch.tanh(self.l3(a)) * self.max_action
+        
+        return a
